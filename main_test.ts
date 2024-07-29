dc51e79f52e1c075b78b7e073cff6387c1449460
Kusaeni <kusaeni@gmail.com>
2024-07-29 18:44:05 +0700

    + test telegram bot

--- main_test.ts
+++ main_test.ts
@@ -23,6 +23,7 @@ Deno.test("database", async (t) => {
     const u = await client.queryObject<NeonData>(
       "INSERT INTO ptldn (post_id, status) VALUES ('testid12345678', 'Testing') RETURNING *",
     );
+    // harusnya failed disini karena post sudah pernah dimasukkan
     assertEquals(u.rows.length, 1);
     assertEquals(u.rows[0].post_id, "testid12345678");
   });
@@ -42,4 +43,26 @@ Deno.test("gotosocial fetch", async (t) => {
   assert(response.status == 200);
   assert(response.redirected == false);
   await response.body?.cancel();
+});
+
+Deno.test("telegram bot test", async (t) => {
+  const tele = await fetch(
+    `https://api.telegram.org/bot${Deno.env.get("TELE_BOT")}/sendMessage`,
+    {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        chat_id: `${Deno.env.get("TELE_CHATID")}`,
+        parse_mode: "markdown",
+        text: `*title message*
+Ini adalah test message`,
+      }),
+    },
+  );
+  assert(tele.ok == true);
+  assert(tele.status == 200);
+  assert(tele.redirected == false);
+  await tele.body?.cancel();
 });
