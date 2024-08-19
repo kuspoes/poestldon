e086d276c4fa6fbb36815526029870107b46ba1c
Kusaeni <kusaeni@gmail.com>
2024-08-19 21:16:20 +0700

    pindah semua code ke dalam try block

--- main.ts
+++ main.ts
@@ -5,45 +5,49 @@ import TurndownService from "turndown";
 const conn = await pool.connect();
 
 async function requestNotif() {
-  const f = await fetch(`${Deno.env.get("GTS_API")}`, {
-    method: "GET",
-    headers: {
-      "Content-Type": "Application/json",
-      Authorization: `Bearer ${Deno.env.get("GTS_TOKEN")}`,
-    },
-  });
+  try {
+    const f = await fetch(`${Deno.env.get("GTS_API")}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "Application/json",
+        Authorization: `Bearer ${Deno.env.get("GTS_TOKEN")}`,
+      },
+    });
 
-  const data = await f.json();
+    const data = await f.json();
 
-  for (const d of data) {
-    const remark: string = "USEND";
-    let inreply: string;
-    let content: string;
-    if (d.type === "follow") {
-      inreply = "null";
-      content = "null";
-    } else {
-      inreply = d.status.in_reply_to_id;
-      content = d.status.content;
+    for (const d of data) {
+      const remark: string = "USEND";
+      let inreply: string;
+      let content: string;
+      if (d.type === "follow") {
+        inreply = "null";
+        content = "null";
+      } else {
+        inreply = d.status.in_reply_to_id;
+        content = d.status.content;
+      }
+
+      if (d.type === "follow") {
+        conn.queryObject<NeonData>`
+            INSERT INTO poestololdon
+            (post_id, created_at, handler, display_name, type, remark)
+            VALUES
+            (${d.id}, ${d.created_at}, ${d.account.acct}, ${d.account.display_name}, ${d.type}, ${remark})
+            ON CONFLICT (post_id) DO NOTHING
+          `;
+      } else {
+        conn.queryObject<NeonData>`
+            INSERT INTO poestololdon
+            (inreplyto, post_id, created_at, handler, display_name, type, status, url, remark)
+            VALUES
+            (${inreply}, ${d.id}, ${d.created_at}, ${d.account.acct}, ${d.account.display_name}, ${d.type}, ${content}, ${d.status.url}, ${remark})
+            ON CONFLICT (post_id) DO NOTHING
+          `;
+      }
     }
-
-    if (d.type === "follow") {
-      conn.queryObject<NeonData>`
-          INSERT INTO poestololdon
-          (post_id, created_at, handler, display_name, type, remark)
-          VALUES
-          (${d.id}, ${d.created_at}, ${d.account.acct}, ${d.account.display_name}, ${d.type}, ${remark})
-          ON CONFLICT (post_id) DO NOTHING
-        `;
-    } else {
-      conn.queryObject<NeonData>`
-          INSERT INTO poestololdon
-          (inreplyto, post_id, created_at, handler, display_name, type, status, url, remark)
-          VALUES
-          (${inreply}, ${d.id}, ${d.created_at}, ${d.account.acct}, ${d.account.display_name}, ${d.type}, ${content}, ${d.status.url}, ${remark})
-          ON CONFLICT (post_id) DO NOTHING
-        `;
-    }
+  } catch (err) {
+    console.log(err);
   }
 }
 
@@ -139,10 +143,14 @@ ${flag}  ${d.type} your post!
 }
 
 async function markNotif() {
-  await conn.queryObject<NeonData>`
-    UPDATE poestololdon
-    SET remark = 'SEND'
-    `;
+  try {
+    await conn.queryObject<NeonData>`
+      UPDATE poestololdon
+      SET remark = 'SEND'
+      `;
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 async function deleteNotif() {
@@ -158,24 +166,28 @@ async function deleteNotif() {
 }
 
 async function sendLogTele(msg: string) {
-  const kapan = Date();
-  await fetch(
-    `https://api.telegram.org/bot${Deno.env.get("TELE_BOT")}/sendMessage`,
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "Application/json",
+  try {
+    const kapan = Date();
+    await fetch(
+      `https://api.telegram.org/bot${Deno.env.get("TELE_BOT")}/sendMessage`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "Application/json",
+        },
+        body: JSON.stringify({
+          chat_id: `${Deno.env.get("TELE_CHATID")}`,
+          parse_mode: "Markdown",
+          text: `🔥 *Info*
+  ${msg}
+  ${kapan}
+          `,
+        }),
       },
-      body: JSON.stringify({
-        chat_id: `${Deno.env.get("TELE_CHATID")}`,
-        parse_mode: "Markdown",
-        text: `🔥 *Info*
-${msg}
-${kapan}
-        `,
-      }),
-    },
-  );
+    );
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 Deno.cron("Sedon-simpan-kirim", "*/3 * * * *", () => {
